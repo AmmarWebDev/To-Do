16d88983f1188ff45eed621e0092f6c18d095ec9
AmmarWebDev <ammarnasr2008@gmail.com>
2023-06-21 07:07:22 +0300

    Added a great improvement to the code.

---
+++ js/row.js
@@ -0,0 +1,11 @@
+let row = `
+  <tr>
+  <td class="count"></td>
+  <td class="check" colspan="2">
+    <input type="checkbox" />
+    <span class="editable" contenteditable></span>
+  </td>
+  </tr>
+`
+
+document.write(row.repeat(100))
