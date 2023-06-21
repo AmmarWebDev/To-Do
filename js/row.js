f1d382748c0ddd9301bcbabaabb2006a13abd83e
AmmarWebDev <ammarnasr2008@gmail.com>
2023-06-21 08:56:45 +0300

    upgrade

--- js/row.js
+++ js/row.js
@@ -1,11 +1,14 @@
+let checkId = Math.floor(Math.random() * 10000);
+
 let row = `
   <tr>
-  <td class="count"></td>
-  <td class="check" colspan="2">
-    <input type="checkbox" />
-    <span class="editable" contenteditable></span>
-  </td>
+    <td class="count"></td>
+    <td class="check" colspan="2">
+      <input type="checkbox" id="checker${checkId}" />
+      <input class="editable" type="text"></input>
+      <label for="checker${checkId}" style="display: none;"></label>
+    </td>
   </tr>
-`
+`;
 
-document.write(row.repeat(100))
+document.write(row.repeat(50));
